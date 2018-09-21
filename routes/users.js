0c26fb1305203cd72287849858d3519458c4e560
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2018-09-21 13:24:32 +0100

    user exist

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,26 @@ const router = express.Router();
 
 router.post('/register', (req, res) => {
     let {username, password} = req.body;
-    let data = {username};
+    User.findOne(username)
+        .then(user =>{
+            if (user !== null) {
+                return res.send({message: "Username exists"});
+            }
+            else {
+                let data = {username};
 
-    let salt = bcrypt.genSaltSync(11);
-    data.password = bcrypt.hashSync(password, salt);
+                let salt = bcrypt.genSaltSync(11);
+                data.password = bcrypt.hashSync(password, salt);
 
-    User.create(data)
-        .then(user => {
-            res.send({message: "user created", user});
+                User.create(data)
+                    .then(user => {
+                        res.send({message: "user created", user});
+                    })
+                    .catch(err => {
+                        return res.status(500).send(err);
+                    });
+            }
+
         })
         .catch(err => {
             return res.status(500).send(err);

2a5a688f04e6a4f4aa2a6f5d01f667e7bd4be4f4
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2018-09-23 08:10:43 +0100

    allow cross origin request

--- bucket.js
+++ bucket.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const users = require('./routes/users');
 const auth = require('./routes/auth');
 const bucketlist= require('./routes/bucketlist');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/api/v1/users', users);
